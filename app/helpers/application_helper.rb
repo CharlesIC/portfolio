2d8ba34a839b7d1c0ca09c12229a74dbf0a217c0
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2018-09-22 02:06:13 +0100

    Add Contact page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def hide_social_media?
+    content_for?(:hide_social_media)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root "pages#welcome"
   get "portfolio" => "pages#portfolio", as: :portfolio
   get "about" => "pages#about", as: :about
+  get "contact" => "pages#contact", as: :contact
 end
